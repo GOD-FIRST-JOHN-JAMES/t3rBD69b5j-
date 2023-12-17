a0760ae0022109c5f438d43cf7574d332a166eac
GOD FIRST JOHN JAMES <133174776+t3rBD69b5j@users.noreply.github.com>
2023-12-17 01:35:49 +0100

    Add files via upload
    Signed-off-by: GOD FIRST JOHN JAMES <133174776+t3rBD69b5j@users.noreply.github.com>

---
+++ VPN pvis.c
@@ -0,0 +1,98 @@
+THANK YOU JESUS CHRIST MY LORD 
+
+
+
+Last updated [11.20, 2021]
+1. Usage Policy
+
+You agree to comply with all applicable laws and regulations in connection with your use of this service.
+You shall not use any Service to engage in, foster, or promote illegal, abusive, fraudulent, or irresponsible behavior, including without limitation:
+Creation or distribution of unsolicited bulk email and mailing lists;
+Disruption or interference of any data system or network, computer or communications system, software application, or network or computing device;
+Use of an Internet account or computer without the owner's authorization;
+Constitute, depict, foster, promote, or relate in any manner to child sexual abuse, bestiality, or non-consensual sex acts;
+Be excessively violent, incite violence, threaten violence, or contain harassing content or hate speech;
+Use our network or services to download, publish, display, distribute, copy or otherwise use or make available in any manner any text, music, software, art, image or other work protected by law unless: (i) you have been expressly authorized by the owner of the property; or (ii) you are otherwise authorized by established Intellectual Property law.
+We reserve the right, in our sole discretion, to refuse service, suspend your account, or restrict access to the App Services. Such suspension or restriction of access may be implemented immediately by the App without any instructions, notice or refund. 
+We are under no obligation to enforce these Terms against you. However, in the event of such a breach, we may act appropriately at our sole discretion. 
+2.Privacy Policy
+
+INTRODUCTION
+
+we respects the privacy of our users . This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website and our mobile application, including any other media form, media channel, mobile website, or mobile application related or connected thereto (collectively, the “Site”). Please read this privacy policy carefully. If you do not agree with the terms of this privacy policy, please do not access the site. 
+We reserve the right to make changes to this Privacy Policy at any time and for any reason. We will alert you about any changes by updating the “Last Updated” date of this Privacy Policy. Any changes or modifications will be effective immediately upon posting the updated Privacy Policy on the Site, and you waive the right to receive specific notice of each such change or modification. 
+You are encouraged to periodically review this Privacy Policy to stay informed of updates. You will be deemed to have been made aware of, will be subject to, and will be deemed to have accepted the changes in any revised Privacy Policy by your continued use of the Site after the date such revised Privacy Policy is posted. 
+COLLECTION OF YOUR INFORMATION We may collect information about you in a variety of ways. The information we may collect on the Site includes:
+Personal Data
+
+Information that you actively provide to us when communicating(Including when you fill out online forms, respond to surveys, provide feedback, participate in promotions), such as your name, email address, phone number, message content and/or attachments you may send to us, and any other information you may choose to provide.
+Account Data
+
+Our application will assign you a unique identifier (This unique identifier is only tied to the application identifier, not to any other personal information of the user). Before establishing a VPN connection, we will use this unique identifier to check your account information to determine the level of service you have.
+Derivative Data
+
+Information our servers collect when you access the Site, such as your browser type, your operating system, your access times. If you are using our mobile application, this information may also include your device name and type, your operating system and your application version numbers.
+Financial Data
+
+Financial information, such as data related to your payment method (e.g. valid credit card number, card brand, expiration date) that We store only very limited, if any, financial information that we collect. Otherwise, all financial information is stored by our payment processor, [Google Checkout], and you are encouraged to review their privacy policy and contact them directly for responses to your questions.
+VPN Data
+
+If you connect using our VPN service:
+We do not collect any information about the websites you visit, nor do we collect any data stored or transmitted on your device, including any data that may be transmitted by the application on your device through the VPN network.
+USE OF YOUR INFORMATION
+
+Having accurate information about you permits us to provide you with a smooth, efficient, and customized experience. Specifically, we may use information collected about you via the Site or our mobile application to: 
+
+Create and manage your account.
+Deliver targeted advertising, coupons, newsletters, and other information regarding promotions and the Site and our mobile application to you.
+Email you regarding your account or order.
+Fulfill and manage purchases, orders, payments, and other transactions related to the Site and our mobile application.
+Generate a personal profile about you to make future visits to the Site and our mobile application more personalized.
+Increase the efficiency and operation of the Site and our mobile application.
+Monitor and analyze usage and trends to improve your experience with the Site and our mobile application.
+Notify you of updates to the Site and our mobile applications.
+Perform other business activities as needed.
+Prevent fraudulent transactions, monitor against theft, and protect against criminal activity.
+Process payments and refunds.
+Request feedback and contact you about your use of the Site and our mobile application .
+Resolve disputes and troubleshoot problems.
+Respond to product and customer service requests.
+Solicit support for the Site and our mobile application.
+Other
+DISCLOSURE OF YOUR INFORMATION
+
+We may share information we have collected about you in certain situations. Your information may be disclosed as follows: 
+By Law or to Protect Rights
+
+If we believe the release of information about you is necessary to respond to legal process, to investigate or remedy potential violations of our policies, or to protect the rights, property, and safety of others, we may share your information as permitted or required by any applicable law, rule, or regulation. This includes exchanging information with other entities for fraud protection and credit risk reduction.
+Third-Party Advertisers
+
+We may use third-party advertising companies to serve ads when you visit the Site or our mobile application. These companies may use information about your visits to the Site and our mobile application and other websites that are contained in web cookies in order to provide advertisements about goods and services of interest to you. 
+TRACKING TECHNOLOGIES
+
+Cookies and Web Beacons
+
+We may use cookies, web beacons, tracking pixels, and other tracking technologies on the Site and our mobile application to help customize the Site and our mobile application and improve your experience. When you access the Site or our mobile application, your personal information is not collected through the use of tracking technology. Most browsers are set to accept cookies by default. You can remove or reject cookies, but be aware that such action could affect the availability and functionality of the Site or our mobile application. You may not decline web beacons. However, they can be rendered ineffective by declining all cookies or by modifying your web browser’s settings to notify you each time a cookie is tendered, permitting you to accept or decline cookies on an individual basis.
+Internet-Based Advertising
+
+Additionally, we may use third-party software to serve ads on the Site and our mobile application. This third-party software may use cookies or similar tracking technology to help manage and optimize your online experience with us. For more information about opting-out of interest-based ads, visit the Network Advertising Initiative Opt-Out Tool or Digital Advertising Alliance Opt-Out Tool.
+THIRD-PARTY WEBSITES
+
+The Site and our mobile application may contain links to third-party websites and applications of interest, including advertisements and external services, that are not affiliated with us. Once you have used these links to leave the Site or our mobile application, any information you provide to these third parties is not covered by this Privacy Policy, and we cannot guarantee the safety and privacy of your information. Before visiting and providing any information to any third-party websites, you should inform yourself of the privacy policies and practices (if any) of the third party responsible for that website, and should take those steps necessary to, in your discretion, protect the privacy of your information. We are not responsible for the content or privacy and security practices and policies of any third parties, including other sites, services or applications that may be linked to or from the Site or our mobile application. 
+SECURITY OF YOUR INFORMATION
+
+We use administrative, technical, and physical security measures to help protect your personal information. While we have taken reasonable steps to secure the personal information you provide to us, please be aware that despite our efforts, no security measures are perfect or impenetrable, and no method of data transmission can be guaranteed against any interception or other type of misuse. Any information disclosed online is vulnerable to interception and misuse by unauthorized parties. Therefore, we cannot guarantee complete security if you provide personal information.
+POLICY FOR CHILDREN
+
+We do not knowingly solicit information from or market to children under the age of 13. If you become aware of any data we have collected from children under age 13, please contact us using the contact information provided below. 
+CALIFORNIA PRIVACY RIGHTS
+
+California Civil Code Section 1798.83, also known as the “Shine The Light” law, permits our users who are California residents to request and obtain from us, once a year and free of charge, information about categories of personal information (if any) we disclosed to third parties for direct marketing purposes and the names and addresses of all third parties with which we shared personal information in the immediately preceding calendar year. If you are a California resident and would like to make such a request, please submit your request in writing to us using the contact information provided below.
+If you are under 18 years of age, reside in California, and have a registered account with the Site or our mobile application, you have the right to request removal of unwanted data that you publicly post on the Site or our mobile application. To request removal of such data, please contact us using the contact information provided below, and include the email address associated with your account and a statement that you reside in California. We will make sure the data is not publicly displayed on the Site or our mobile application, but please be aware that the data may not be completely or comprehensively removed from our systems.
+CONTACT US
+
+If you have questions or comments about this Privacy Policy, please contact us at
+
+Email:johneavl11@gmail.com .xyz
+    THANK YOU JESUS CHRIST MY LORD 💖
+       JESUS CHRIST IS 
